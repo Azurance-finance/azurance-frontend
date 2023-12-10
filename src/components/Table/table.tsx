d52da3c7c63aacfaa195fb04b4e3405bbac3be8f
born-sorravit <62236649+born-sorravit@users.noreply.github.com>
2023-12-10 14:35:43 +0700

    🐞 fix: get image url from firebase

--- src/components/Table/table.tsx
+++ src/components/Table/table.tsx
@@ -19,9 +19,12 @@ import { InsuranceType } from "@/store/insurance/insurance.type";
 import { ethers } from "ethers";
 
 export default function AzuranceTable() {
-
   const [filter, setFilter] = useState("Ongoing");
-  const { insurances: insuranceList, fetchInsurances } = useInsurances(100, 0, filter);
+  const { insurances: insuranceList, fetchInsurances } = useInsurances(
+    100,
+    0,
+    filter
+  );
 
   const { currentChainId } = useWalletStore();
   const { favorites, addFavorite, removeFavorite } = useFavoriteStore();
@@ -33,23 +36,29 @@ export default function AzuranceTable() {
 
   const selectedInsurance = insuranceList ? insuranceList[selectedIndex] : null;
 
-  // TODO: Fix logo image template 
-  const getDownloadURLWithBackup = useCallback(async (chainId: string, address: string) => {
-    try {
-      return await getDownloadURL(ref(storage, `files/${chainId}-${address}.png`));
-    } catch (e) {
-      return `/insurances/AIA.png`
-    }
-  }, []);
+  const getDownloadURLWithBackup = useCallback(
+    async (chainId: string, address: string) => {
+      try {
+        return await getDownloadURL(
+          ref(storage, `files/${chainId}-${address}.png`)
+        );
+      } catch (e) {
+        return `/insurances/AIA.png`;
+      }
+    },
+    []
+  );
 
   useEffect(() => {
     fetchInsurances();
-  }, [filter])
+  }, [filter]);
 
   useEffect(() => {
-    const promises = insuranceList.map(insurance => getDownloadURLWithBackup(currentChainId, insurance.id));
-    Promise.all(promises).then(result => setImageUrls(result))
-  }, [insuranceList, currentChainId])
+    const promises = insuranceList.map((insurance) =>
+      getDownloadURLWithBackup(currentChainId, insurance.id)
+    );
+    Promise.all(promises).then((result) => setImageUrls(result));
+  }, [insuranceList, currentChainId]);
 
   const data = useMemo(() => {
     if (search) {
@@ -66,7 +75,7 @@ export default function AzuranceTable() {
   const handleSelect = (index: number) => {
     setSelectedIndex(index);
     onOpen();
-  }
+  };
 
   const renderFavorite = (id: string) => {
     if (favorites.find((item) => item.id === id)) {
@@ -86,31 +95,41 @@ export default function AzuranceTable() {
 
   const getSellerShare = (index: number) => {
     const insurance = insuranceList[index];
-    return Number(ethers.utils.formatEther(insurance.sellerShares))
-  }
+    return Number(ethers.utils.formatEther(insurance.sellerShares));
+  };
 
   const getBuyerShare = (index: number) => {
     const insurance = insuranceList[index];
-    return Number(ethers.utils.formatEther(insurance.buyerShares))
-  }
+    return Number(ethers.utils.formatEther(insurance.buyerShares));
+  };
 
   const getMultiplier = (index: number) => {
     const insurance = insuranceList[index];
-    return Number(ethers.utils.formatUnits(insurance.multiplier, insurance.multiplierDecimals));
-  }
+    return Number(
+      ethers.utils.formatUnits(
+        insurance.multiplier,
+        insurance.multiplierDecimals
+      )
+    );
+  };
 
   const getTotalValue = (index: number) => {
     const insurance = insuranceList[index];
-    return Number(ethers.utils.formatUnits(insurance.totalShares, insurance.underlyingToken.decimals));
-  }
+    return Number(
+      ethers.utils.formatUnits(
+        insurance.totalShares,
+        insurance.underlyingToken.decimals
+      )
+    );
+  };
 
   const getUtilization = (index: number) => {
-    const sellerShare = getSellerShare(index)
-    const buyerShare = getBuyerShare(index)
-    const multiplier = getMultiplier(index)
+    const sellerShare = getSellerShare(index);
+    const buyerShare = getBuyerShare(index);
+    const multiplier = getMultiplier(index);
     if (sellerShare === 0) return 0;
-    return (buyerShare * multiplier / (sellerShare)) * 100
-  }
+    return ((buyerShare * multiplier) / sellerShare) * 100;
+  };
 
   return (
     <div className="bg-white w-full mt-5 rounded-xl">
@@ -134,8 +153,9 @@ export default function AzuranceTable() {
               <td
                 key={column.field}
                 width={column.width}
-                className={`${column.field === "expiration" ? "text-center" : "text-start"
-                  }`}
+                className={`${
+                  column.field === "expiration" ? "text-center" : "text-start"
+                }`}
               >
                 {column.headerName}
               </td>
@@ -148,84 +168,87 @@ export default function AzuranceTable() {
         <tbody className="text-center">
           {data?.length
             ? data
-              .sort((a: InsuranceType, b: InsuranceType) => {
-                return b.createdAt - a.createdAt;
-              })
-              .map((item: InsuranceType, index: number) => (
-                <tr
-                  key={index}
-                  className={`${index !== data.length - 1 && `border-b `
+                .sort((a: InsuranceType, b: InsuranceType) => {
+                  return b.createdAt - a.createdAt;
+                })
+                .map((item: InsuranceType, index: number) => (
+                  <tr
+                    key={index}
+                    className={`${
+                      index !== data.length - 1 && `border-b `
                     } h-[95px] text-gray-600 rounded-none hover:bg-gray-50`}
-                >
-                  <td>
-                    <div
-                      className="flex w-full justify-center items-center"
-                      onClick={() => {
-                        handleFavorite(item);
-                      }}
-                    >
-                      {renderFavorite(item.id)}
-                    </div>
-                  </td>
-                  <td className="text-start">
-                    <div className="flex">
-                      <picture className="flex items-center">
-                        <img
-                          src={imageUrls[index] || "/insurances/AIA.png"}
-                          width="36px"
-                          height="36px"
-                          alt="logo-chain"
-                        />
-                      </picture>
-                      <div className="text-start ml-2">
-                        <div className="text-sm font-semibold  text-[#0F1419]">
-                          {item.name}
-                        </div>
-                        <div className="max-w-[30px] text-[10px] bg-[#E5E5E6] rounded text-center py-[2px]">
-                          {item.symbol}
+                  >
+                    <td>
+                      <div
+                        className="flex w-full justify-center items-center"
+                        onClick={() => {
+                          handleFavorite(item);
+                        }}
+                      >
+                        {renderFavorite(item.id)}
+                      </div>
+                    </td>
+                    <td className="text-start">
+                      <div className="flex">
+                        <picture className="flex items-center">
+                          <img
+                            src={imageUrls[index] || "/insurances/AIA.png"}
+                            width="36px"
+                            height="36px"
+                            alt="logo-chain"
+                          />
+                        </picture>
+                        <div className="text-start ml-2">
+                          <div className="text-sm font-semibold  text-[#0F1419]">
+                            {item.name}
+                          </div>
+                          <div className="max-w-[30px] text-[10px] bg-[#E5E5E6] rounded text-center py-[2px]">
+                            {item.symbol}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </td>
-                  <td className="text-start  text-[#0F1419]">{getMultiplier(index)}x</td>
-                  <td className="text-start">
-                    <PercentageBar utilization={getUtilization(index)} totalBuyer={getBuyerShare(index)} totalSeller={getSellerShare(index)} />
-                  </td>
-                  <td className="text-start  text-[#0F1419]">
-                    {formatDecimal(getTotalValue(index))}
-                  </td>
-                  <td>
-                    <div className="flex flex-col justify-center items-center my-auto ">
-                      <TimeRemine timeData={item.maturityTime} />
-                    </div>
-                  </td>
-                  <td className="text-start px-4">
-                    <Button
-                      color="primary"
-                      className=" px-4"
-                      onClick={() => handleSelect(index)}
-                    >
-                      Buy
-                    </Button>
-                  </td>
-                </tr>
-              ))
+                    </td>
+                    <td className="text-start  text-[#0F1419]">
+                      {getMultiplier(index)}x
+                    </td>
+                    <td className="text-start">
+                      <PercentageBar
+                        utilization={getUtilization(index)}
+                        totalBuyer={getBuyerShare(index)}
+                        totalSeller={getSellerShare(index)}
+                      />
+                    </td>
+                    <td className="text-start  text-[#0F1419]">
+                      {formatDecimal(getTotalValue(index))}
+                    </td>
+                    <td>
+                      <div className="flex flex-col justify-center items-center my-auto ">
+                        <TimeRemine timeData={item.maturityTime} />
+                      </div>
+                    </td>
+                    <td className="text-start px-4">
+                      <Button
+                        color="primary"
+                        className=" px-4"
+                        onClick={() => handleSelect(index)}
+                      >
+                        Buy
+                      </Button>
+                    </td>
+                  </tr>
+                ))
             : null}
         </tbody>
       </table>
-      {
-        selectedInsurance && (
-          <StakeModal
-            isOpen={isOpen}
-            insurance={selectedInsurance}
-            header="Buy"
-            onOpenChange={onOpenChange}
-            onInsuranceUpdate={fetchInsurances}
-          />
-        )
-      }
-
+      {selectedInsurance && (
+        <StakeModal
+          isOpen={isOpen}
+          insurance={selectedInsurance}
+          header="Buy"
+          onOpenChange={onOpenChange}
+          onInsuranceUpdate={fetchInsurances}
+        />
+      )}
     </div>
   );
 }
-
